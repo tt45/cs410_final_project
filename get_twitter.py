95911e2e08a6d55779d901e171b0944357b1cd87
tt45 <thompsonteng@gmail.com>
2018-04-17 20:56:05 -0500

    w+

--- get_twitter.py
+++ get_twitter.py
@@ -2,21 +2,21 @@ import tweepy
 import csv
 import pandas as pd
 ####input your credentials here
-consumer_key =  ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+consumer_key =  'AwZGVU4TtZwg07Jfr2jjU8D7d'
+consumer_secret = 'iCRFJEvj0vmb4E2dL2syjxjg9lGzEOEGd3M2tbjha8cBZBZ4ZT'
+access_token = '919999923663360000-OjZOyOm6i5KQPp1gmBzACk5rOCmJqV2'
+access_token_secret = 'DtBh2dfDVvlFEVRmYgRSoMvpvfyaeNyf0RzWNWFAPK74w'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 #####United Airlines
 # Open/Create a file to append data
-csvFile = open('ua.csv', 'a')
+csvFile = open('ua.csv', 'w+')
 #Use csv Writer
 csvWriter = csv.writer(csvFile)
 
-for tweet in tweepy.Cursor(api.search,q="#amazonstock",count=100,
+for tweet in tweepy.Cursor(api.search,q="#unitedAIRLINES",count=100,
                            lang="en",
                            since="2017-04-03").items():
     print (tweet.created_at, tweet.text)
